c38cab984006e86ac2b1734ba1d1f42f9ff3b2a6
eva-lm <eva.lm1810@gmail.com>
2019-09-23 12:43:46 +0200

    add improvements to the router

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -7,22 +7,34 @@ const Detail = props => {
   const filmId = routerProps.match.params.id;
 
   const filmDetail = films.filter(item => item.id === filmId);
-  const { title, image, release_date, description } = filmDetail[0];
-  return (
-    <React.Fragment>
-      <div className="film_detail">
-        <h2 className="film-detail__title">{title}</h2>
-        <div className="film-detail__image">
-          <img src={image} alt={title} />
+
+  if (filmDetail[0]) {
+    const { title, image, release_date, description } = filmDetail[0];
+    return (
+      <React.Fragment>
+        <div className="film_detail">
+          <h2 className="film-detail__title">{title}</h2>
+          <div className="film-detail__image">
+            <img src={image} alt={title} />
+          </div>
+          <p className="film-detail__date">{release_date}</p>
+          <p className="film-detail__description">{description}</p>
         </div>
-        <p className="film-detail__date">{release_date}</p>
-        <p className="film-detail__description">{description}</p>
-      </div>
-      <Link to="/" className="app__back">
-        Back to List
-      </Link>
-    </React.Fragment>
-  );
+        <Link to="/" className="app__back">
+          Back to List
+        </Link>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <p>Esta página no existe</p>
+        <Link to="/" className="app__back">
+          Back to List
+        </Link>
+      </React.Fragment>
+    );
+  }
 };
 
 Detail.propTypes = {
